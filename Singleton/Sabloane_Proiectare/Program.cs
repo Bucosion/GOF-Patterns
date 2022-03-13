2b1c27e2e3aa90b847273be1482efd13871ebc3d
Bucosion <82382840+Bucosion@users.noreply.github.com>
2022-03-13 18:40:27 +0300

    Add files via upload

---
+++ Singleton/Sabloane_Proiectare/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Sabloane_Proiectare
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               int x = 0, y = 0;
+
+               //while (y <= 5)
+               //{
+               //     Singleton.GetInstance().Move(x);
+               //     x++;
+               //     y++;
+               //}
+
+               Console.WriteLine("Testează Task-ul!");
+
+               Task.Run(() => Singleton.GetInstance().Move(1));
+               Task.Run(() => Singleton.GetInstance().Move(2));
+               Console.ReadLine();
+          }
+     }
+}
---
+++ Singleton/Sabloane_Proiectare/Singleton.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabloane_Proiectare
+{
+     class Singleton
+     {
+          private static Singleton _instance;  
+          private static object locker = new object();
+          public static void Print()
+          {
+               Console.WriteLine("Bună!");
+          }
+
+          public void Move(int x)
+          {
+               if (x % 2 == 0)
+               {
+                    Console.WriteLine("Jucatorul alb a mutat!");
+               }
+               else
+               {
+                    Console.WriteLine("Jucatorul negru a a mutat!");
+               }
+          }
+
+          private Singleton()  
+          {
+               Console.WriteLine("Tabla a fost creata cu succes");
+          }
+
+          public static Singleton GetInstance() 
+          {
+               if (_instance == null) 
+               {
+                    lock (locker)
+                    {
+                         if (_instance == null)
+                         {
+                              _instance = new Singleton();
+                         }
+                    }
+               }
+               return _instance;
+          }
+     }
+}
